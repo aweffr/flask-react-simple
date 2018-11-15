6e9e8bd1a3df8c00791aa9a78ee70d199dcab420
aweffr <aweffr@qq.com>
2018-11-15 15:21:04 +0800

    add github url

--- flask_reactjs/__init__.py
+++ flask_reactjs/__init__.py
@@ -2,3 +2,34 @@
 # encoding=utf-8
 
 from .__main__ import create
+from flask import current_app, _app_ctx_stack
+
+
+class FlaskReact(object):
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        self.app = app
+        app.config.setdefault('BABEL_ENV_TARGET', '> 0.5%, last 2 versions, Firefox ESR, not dead')
+
+    def inject_page_source(self):
+        raise NotImplemented
+
+    def teardown(self):
+        ctx = _app_ctx_stack.top
+        if hasattr(ctx, 'react'):
+            # Do clean staff
+            ctx.react = None
+
+    @property
+    def sources(self):
+        ctx = _app_ctx_stack.top
+        if ctx is not None:
+            if not hasattr(ctx, 'react'):
+                ctx.react = {
+                    'sources': ['1.js', '2.js']
+                }
+            return ctx.react['sources']
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     long_description=open('README.rst', encoding='utf-8').read(),
     author='aweffr',
     author_email='aweffr@foxmail.com',
+    url='https://github.com/aweffr/flask-react-simple',
     python_requires='>=3.6',
     include_package_data=True,  # specify data files to be included in your packages
     install_requires=[
